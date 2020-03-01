8bc0b77ff98c58bd7cf066a1e82b85e4f6ab5ffa
10xOXR <10xoxr@gmail.com>
2020-03-01 11:43:10 +0000

    Add map marker

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,10 +1,18 @@
 function initMap() {
+
     const map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10,
         center: {
             lat: 53.350140,
             lng: -6.266155
         }
+    });
+
+    const marker = new google.maps.Marker({
+        position: {lat: 53.346464, lng: -6.306677},
+        map: map,
+        animation: google.maps.Animation.DROP,
+        title: 'Where ze bears are!'
     });
 }
 
