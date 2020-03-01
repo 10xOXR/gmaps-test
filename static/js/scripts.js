9df10774fc83baba975103ffb82e4ac0b1351f90
10xOXR <10xoxr@gmail.com>
2020-03-01 12:05:43 +0000

    Add marker toggle feature

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,6 +1,9 @@
+let map;
+let marker;
+
 function initMap() {
 
-    const map = new google.maps.Map(document.getElementById("map"), {
+    map = new google.maps.Map(document.getElementById("map"), {
         zoom: 10,
         center: {
             lat: 53.350140,
@@ -8,12 +11,24 @@ function initMap() {
         }
     });
 
-    const marker = new google.maps.Marker({
-        position: {lat: 53.346464, lng: -6.306677},
-        map: map,
+    marker = new google.maps.Marker({
+        position: {
+            lat: 53.346464,
+            lng: -6.306677
+        },
         animation: google.maps.Animation.DROP,
         title: 'Where ze bears are!'
     });
 }
 
 initMap();
+
+let checkbox = document.querySelector("input[name=showMarker]");
+
+checkbox.addEventListener('change', () => {
+    if (checkbox.checked) {
+        marker.setMap(map);
+    } else {
+        marker.setMap(null);
+    }
+});
